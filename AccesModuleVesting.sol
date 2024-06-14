edcf91d57c5e8bac52e2c1fc3d2f520776fb3cb9
Jerffry <85122030+Jaafarkrayem@users.noreply.github.com>
2024-06-14 15:30:05 +0300

    Update AccesModuleVesting.sol

--- AccesModuleVesting.sol
+++ AccesModuleVesting.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import "./IAccesDeployer.sol";
-import "./AccesVesting_V2.sol";
+import {IAccesDeployer} from "./IAccesDeployer.sol";
+import {AccesVesting_V2} from "./AccesVesting_V2.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 
 contract AccesModuleVesting {
     IAccesDeployer public deployer;
@@ -11,7 +13,7 @@ contract AccesModuleVesting {
     address payable public addressOfVestingContract;
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "not owner");
+        require(msg.sender == owner, "Module: not owner");
         _;
     }
 
@@ -25,17 +27,15 @@ contract AccesModuleVesting {
     }
 
     function deployVestingContract(
-        ERC20 _Acces,
-        address timelock
+        IERC20 _Acces
     ) public onlyOwner {
         require(addressOfVestingContract == address(0), "Vesting contract already deployed");
 
-        AccesVesting_V2 vestingContract = new AccesVesting_V2(_Acces, timelock, address(deployer));
+        AccesVesting_V2 vestingContract = new AccesVesting_V2(_Acces, address(deployer));
         addressOfVestingContract = payable(address(vestingContract));
     }
 
     function addInvestorInVesting(
-        address _module,
         address _investor, 
         bool _isTeam, 
         uint256 _amount, 
@@ -48,58 +48,50 @@ contract AccesModuleVesting {
             _investor, _isTeam, _amount, _yPercent, _lockTime, _firstUnlock
         );
 
-        deployer.submitTransactionFromModule(msg.sender, _module, 0, data);
+        deployer.submitTransactionFromModule(msg.sender, addressOfVestingContract, 0, data);
     }
 
-    function setMonthlyPercentage(address _module, uint256 _mP) public onlyOwner {
+    function setMonthlyPercentage(uint256 _mP) public onlyOwner {
         bytes memory data = abi.encodeWithSignature(
             "setMonthlyPercentage(uint256)",
             _mP
         );
 
-        deployer.submitTransactionFromModule(msg.sender, _module, 0, data);
+        deployer.submitTransactionFromModule(msg.sender, addressOfVestingContract, 0, data);
     }
 
-    function addToBlackList(address _module, address _investor) public onlyOwner {
+    function addToBlackList(address _investor) public onlyOwner {
         bytes memory data = abi.encodeWithSignature(
             "addToBlackList(address)",
             _investor
         );
 
-        deployer.submitTransactionFromModule(msg.sender, _module, 0, data);
+        deployer.submitTransactionFromModule(msg.sender, addressOfVestingContract, 0, data);
     }
 
-    function removeFromBlackList(address _module, address _investor) public onlyOwner {
+    function removeFromBlackList(address _investor) public onlyOwner {
         bytes memory data = abi.encodeWithSignature(
             "removeFromBlackList(address)",
             _investor
         );
 
-        deployer.submitTransactionFromModule(msg.sender, _module, 0, data);
+        deployer.submitTransactionFromModule(msg.sender, addressOfVestingContract, 0, data);
     }
 
-    function activateSamePercentage(address _module) public onlyOwner {
+    function activateSamePercentage() public onlyOwner {
         bytes memory data = abi.encodeWithSignature(
             "activateSamePercentage()"
         );
 
-        deployer.submitTransactionFromModule(msg.sender, _module, 0, data);
+        deployer.submitTransactionFromModule(msg.sender, addressOfVestingContract, 0, data);
     }
 
-    function disableSamePercentage(address _module) public onlyOwner {
+    function disableSamePercentage() public onlyOwner {
         bytes memory data = abi.encodeWithSignature(
             "disableSamePercentage()"
         );
 
-        deployer.submitTransactionFromModule(msg.sender, _module, 0, data);
+        deployer.submitTransactionFromModule(msg.sender, addressOfVestingContract, 0, data);
     }
 
-    function editInvestorLock(address _module, address _investor, uint256 _yPercent, uint256 _mPercent) public onlyOwner {
-        bytes memory data = abi.encodeWithSignature(
-            "editInvestorLock(address,uint256,uint256)",
-            _investor, _yPercent, _mPercent
-        );
-
-        deployer.submitTransactionFromModule(msg.sender, _module, 0, data);
-    }
 }
